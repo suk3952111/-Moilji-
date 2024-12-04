955bdb159b1216f382f51a67a32950df9c8a4cfe
suk3952111 <suk3952111@naver.com>
2024-12-04 16:34:28 +0900

    chore: 주석 제거

--- app/auth/signup/_components/form.tsx
+++ app/auth/signup/_components/form.tsx
@@ -7,8 +7,7 @@ import { FormInput } from './form-input';
 
 export const Form = () => {
   const initialState = { message: '', errors: {} };
-  const [state, dispatch] = useFormState(userSignup, initialState); // createSignup은 /src/app/signup/lib/signup.ts의 함수
-
+  const [state, dispatch] = useFormState(userSignup, initialState);
   return (
     <form action={dispatch}>
       <div className='flex flex-col space-y-2'>
