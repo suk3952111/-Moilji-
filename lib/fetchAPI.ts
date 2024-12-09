b13dd8380969506154887286e36ac2db2aa9cc79
suk3952111 <suk3952111@naver.com>
2024-12-09 18:38:22 +0900

    feat: 클라이언트 와 서버 환경에서 쿠키토큰 받아오는 방법 세분화 (#18)

--- lib/fetchAPI.ts
+++ lib/fetchAPI.ts
@@ -1,23 +1,30 @@
 import Cookies from 'js-cookie';
+import { cookies as serverCookies } from 'next/headers';
 
-//env 추가되면 정리 예정
 const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN || 'http://54.180.31.176';
+
+// 쿠키 가져오기 (클라이언트/서버 환경 구분)
+function getToken(): string | undefined {
+  if (typeof window !== 'undefined') {
+    // 클라이언트 환경
+    return Cookies.get('token');
+  } else {
+    // 서버 환경
+    const cookieStore = serverCookies();
+    return cookieStore.get('token')?.value;
+  }
+}
 
 export async function fetchAPI(
   endpoint: string,
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
   body?: FormData | string | Record<string, unknown>,
 ) {
-  const token = Cookies.get('token');
-
-  // if (!token) {
-  //   window.location.href = '/login';
-  //   return; // 일단은 이렇게 하고 나중에 router.push나 nextResponse.redirect로 구현해볼 예정
-  // }
+  const token = getToken();
 
   const headers: HeadersInit = {
     accept: '*/*',
-    Authorization: `Bearer ${token}`,
+    ...(token && { Authorization: `Bearer ${token}` }),
     ...(body &&
       !(body instanceof FormData) && { 'Content-Type': 'application/json' }),
   };
@@ -34,14 +41,16 @@ export async function fetchAPI(
     const response = await fetch(`${BASE_URL}${endpoint}`, options);
 
     if (!response.ok) {
-      const errorMessage = await response.text();
-      const error = new Error(errorMessage || 'Network response was not ok');
+      const error = {
+        status: response.status,
+        message: (await response.text()) || 'Network response was not ok',
+      };
       throw error;
     }
 
     return response.status !== 204 ? await response.json() : null;
   } catch (error) {
-    console.error('API 호출 에러:', error); //테스트용
+    console.error('API 호출 에러:', error); // 로깅 추가 가능
     throw error;
   }
 }
